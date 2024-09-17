bb34caf14ee764b8ecb7a0800c424de690d7f62f
benhensor <benhensor@gmail.com>
2024-09-17 16:54:07 +0100

    api test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [helloWorld, setHelloWorld] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('https://scamazon-server.vercel.app/api/test');
+      setHelloWorld(response.data.hello);
+    }
+    fetchData();
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{helloWorld}</h1>
     </div>
   );
 }
